d28a0481ce917d89467bf013e60c38f73a2e5e5b
Andrei Poleschuk <renardby@mail.ru>
2018-09-16 11:21:37 +0300

    task 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/\s/g, '') + ' '; 
+
+  let answers = []; 
+  let stringSecond = equation.slice(equation.indexOf('x^2')+3, -1) + ' ';
+  let stringThird = stringSecond.slice(stringSecond.indexOf('*x')+2,-1);
+  
+  let A = parseInt(equation,10);
+  let B = parseInt(stringSecond, 10);
+  let C = parseInt(stringThird, 10);
+
+  answers.push(Math.round(( - B - Math.sqrt(B*B-4*A*C)) / (2*A)));
+  answers.push(Math.round(( - B + Math.sqrt(B*B-4*A*C)) / (2*A)));
+  answers.sort((a,b) => a-b);
+
+  return answers;
 }
